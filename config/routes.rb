a4612613c3f3e045ba378837c7505c975b91a718
MichaelFlanagan <Michael.flanagan87@gmail.com>
2014-01-22 23:11:34 +0000

    Part 4 commit  set up the application home page  end part 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 NkuRails::Application.routes.draw do
+   resources :posts
+ 
+  root  to:"welcome#index"
+#end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
+    #root "welcome#index"
+     
+    
+  
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
