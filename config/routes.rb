bb258b10b3a4e91185b1e7efacfb9118f75f8581
MichaelFlanagan <Michael.flanagan87@gmail.com>
2014-01-23 21:12:38 +0000

    Added form for New posts, in part 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 NkuRails::Application.routes.draw do
-   resources :posts
+  get "welcome/index"
+  
+  resources :posts
+    root to: "welcome#index"
+
  
-  root  to:"welcome#index"
 #end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
